17f3e83faa8211a8cb115f0912d22724034af65b
Code Jury <119634865+Code-Jury@users.noreply.github.com>
2024-01-29 18:36:49 -0700

    Add files via upload
    Version  3 of the Website

---
+++ E-Commerce - SHOPitALL/js/script.js
@@ -0,0 +1,6 @@
+function categories() {
+    cat1 = "Laptops", cat1 = "Phones & Tablets", cat1 = "Groceries", cat1 = "Electronics", cat1 = "Clothing & Wears";
+     cat1 = "Home Appliances", cat1 = "Cars & Automobiles", cat1 = "Health", cat1 = "Books & Articles", cat1 = "Services";
+     cat1 = "Men's Fashion", cat1 = "Women's Fashion", cat1 = "Toys, Kids & Babies", cat1 = "Bags & Shoes";
+
+   }
